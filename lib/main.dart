2a0cc733f2383974779b134aba753163035d4130
GabrielDeml <gabrielddeml@gmail.com>
2021-10-21 10:32:09 -0400

    It builds

--- lib/main.dart
+++ lib/main.dart
@@ -127,9 +127,9 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: 
-      // Text("hello"),
-      Column(
+      body:
+          // Text("hello"),
+          Column(
         children: [
           TableCalendar<Event>(
             firstDay: kFirstDay,
